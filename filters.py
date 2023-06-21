f87d5a53b06fa81a9d04014d9e52b0f0c92478cd
namvt002 <viettrung0601@gmail.com>
2023-06-21 14:03:22 +0700

    format code

--- filters.py
+++ filters.py
@@ -1,67 +1,97 @@
+"""You'll edit this file in Tasks 3a and 3c."""
 import operator
 import itertools
+
+
 class UnsupportedCriterionError(NotImplementedError):
     """A filter criterion is unsupported."""
 
 
 class AttributeFilter:
+    """A general superclass for filters on comparable attributes."""
 
     def __init__(self, op, value):
+        """Construct a new `AttributeFilter` from an binary predicate and a reference value."""
         self.op = op
         self.value = value
 
     def __call__(self, approach):
+        """Invoke `self(approach)`."""
         return self.op(self.get(approach), self.value)
 
     @classmethod
     def get(cls, approach):
+        """Get an attribute of interest from a close approach.
+
+        Concrete subclasses must override this method to get an attribute of
+        interest from the supplied `CloseApproach`.
+
+        :param approach: A `CloseApproach` on which to evaluate this filter.
+        :return: The value of an attribute of interest, comparable to `self.value` via `self.op`.
+        """
         raise UnsupportedCriterionError
 
     def __repr__(self):
-        return f"{self.__class__.__name__}(op=operator.{self.op.__name__}, value={self.value})"
+        """Use to return a formatted string that displays the class name."""
+        return f"{self.__class__.__name__}"\
+                    f"(op=operator.{self.op.__name__}, value={self.value})"
 
-# class DesignationFilter(AttributeFilter):
-#     @classmethod
-#     def get(cls, approach):
-#         return approach.neo.designation
 
 class DateFilter(AttributeFilter):
+    """A filter DateFilter."""
+
     @classmethod
     def get(cls, approach):
+        """Return the date."""
         return approach.time.date()
-    
+
+
 class DistanceFilter(AttributeFilter):
+    """A filter that DistanceFilter."""
+
     @classmethod
     def get(cls, approach):
+        """Return the distance."""
         return approach.distance
-    
+
+
 class VelocityFilter(AttributeFilter):
+    """A filter that VelocityFilter."""
+
     @classmethod
     def get(cls, approach):
+        """Return the velocity."""
         return approach.velocity
-    
+
+
 class DiameterFilter(AttributeFilter):
+    """A filter that DiameterFilter."""
+
     @classmethod
     def get(cls, approach):
+        """Return the diameter."""
         return approach.neo.diameter
 
+
 class HazardousFilter(AttributeFilter):
+    """A filter that HazardousFilter."""
+
     @classmethod
     def get(cls, approach):
-        return approach.neo.hazardous    
+        """Return the hazardous."""
+        return approach.neo.hazardous
+
 
 def create_filters(
-        date=None, start_date=None, end_date=None,
+        date=None, start_date=None,
+        end_date=None,
         distance_min=None, distance_max=None,
         velocity_min=None, velocity_max=None,
         diameter_min=None, diameter_max=None,
         hazardous=None
 ):
-   
+    """Create a collection of filters from user-specified criteria."""
     filters = []
-    #eq ==
-    #ge >=
-    #le <=
     if date:
         filters.append(DateFilter(operator.eq, date))
     if start_date:
@@ -82,10 +112,12 @@ def create_filters(
         filters.append(DiameterFilter(operator.le, diameter_max))
     if hazardous is not None:
         filters.append(HazardousFilter(operator.eq, hazardous))
+
     return filters
 
-def limit(iterator, n=None):
 
+def limit(iterator, n=None):
+    """Limit the."""
     if not n:
         return iterator
     else:
